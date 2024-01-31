459867ea30d3ca769325b6eb32e4962913fceb73
murilobeltrame <murilobeltrame@gmail.com>
2024-01-30 23:49:50 -0300

    ...

--- CleanArchByMe/src/CleanArchByMe.Infrastructure/Data/ApplicationContext.cs
+++ CleanArchByMe/src/CleanArchByMe.Infrastructure/Data/ApplicationContext.cs
@@ -5,4 +5,11 @@ using Microsoft.EntityFrameworkCore;
 namespace CleanArchByMe.Infrastructure.Data;
 
 [ExcludeFromCodeCoverage]
-public class ApplicationContext(DbContextOptions options) : DbContext(options);
+public class ApplicationContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ CleanArchByMe/src/CleanArchByMe.Infrastructure/Data/Configurations/TodoConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchByMe.Domain.TodoAggregate;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchByMe.Infrastructure.Data.Configurations;
+
+public class TodoConfiguration : IEntityTypeConfiguration<Todo>
+{
+    public void Configure(EntityTypeBuilder<Todo> builder)
+    {
+        builder.HasKey(t => t.Id);
+        builder.Property(p => p.Title)
+            .IsRequired()
+            .HasMaxLength(50);
+        builder.Property(p => p.Description)
+            .IsRequired()
+            .HasMaxLength(255);
+    }
+}
