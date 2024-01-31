753578181b2e9d777497b31815d6b8f87a798a6b
murilobeltrame <murilobeltrame@gmail.com>
2024-01-30 23:33:58 -0300

    ...

--- CleanArchByMe/tools/CleanArchByMe.Infrastructure.Tools/ApplicationMigrationContext.cs
+++ CleanArchByMe/tools/CleanArchByMe.Infrastructure.Tools/ApplicationMigrationContext.cs
@@ -1,7 +1,7 @@
 using CleanArchByMe.Infrastructure.Data;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace CleanArchByMe.Infrastructure.Tools;
 
-public class ApplicationMigrationContext : ApplicationContext {
-
-}
+public class ApplicationMigrationContext(DbContextOptions options) : ApplicationContext(options) { }
--- CleanArchByMe/tools/CleanArchByMe.Infrastructure.Tools/Program.cs
+++ CleanArchByMe/tools/CleanArchByMe.Infrastructure.Tools/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CleanArchByMe.Infrastructure.Tools;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var app = Host.CreateDefaultBuilder()
+    .ConfigureServices((context,  services) =>
+    {
+        var connectionString = context.Configuration.GetConnectionString("DbConnection");
+        services.AddDbContext<ApplicationMigrationContext>(options => options.UseSqlServer(connectionString));
+    })
+    .Build();
+
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<ApplicationMigrationContext>();
+context.Database.Migrate();
